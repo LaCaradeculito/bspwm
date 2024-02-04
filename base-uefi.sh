ec965399b63051a8742c47121d3ac44e1281c6bb
Marcelo Santorelli <45903722+LaCaradeculito@users.noreply.github.com>
2024-02-04 07:49:33 -0300

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -5,7 +5,7 @@ hwclock --systohc
 sed -i '178s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=es-latin1" >> /etc/vconsole.conf
+echo "KEYMAP=la-latin1" >> /etc/vconsole.conf
 echo "alumno" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
