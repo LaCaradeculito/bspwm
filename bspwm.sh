4a558940e70adbca6214a9a877c5cb4d1199319b
Marcelo Santorelli <45903722+LaCaradeculito@users.noreply.github.com>
2024-02-04 08:21:32 -0300

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -13,7 +13,7 @@ echo "MAIN PACKAGES"
 
 sleep 5
 
-sudo pacman -S --noconfirm xorg xorg-xinit bspwm sxhkd firefox alacritty picom nitrogen lxappearance dmenu clipmenu xorg-xrandr ranger arandr alsa-utils alsa-utils arc-gtk-theme arc-icon-theme dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome awesome-terminal-fonts playerctl scrot dunst pacman-contrib
+sudo pacman -S --noconfirm xorg xorg-xinit bspwm sxhkd firefox alacritty picom nitrogen lxappearance dmenu clipmenu xorg-xrandr ranger arandr alsa-utils alsa-utils arc-gtk-theme arc-icon-theme ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto adobe-source-code-pro-fonts cantarell-fonts playerctl scrot dunst pacman-contrib
 
 mkdir -p .config/{bspwm,sxhkd,dunst}
 
